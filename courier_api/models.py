d3e62e856a57b36bb7ffc92e39a34833485c2374
onise2001 <onise.babulashvili@yahoo.com>
2024-06-07 18:15:56 +0400

    Created inital project files

---
+++ courier_api/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Parcel(models.Model):
+    ...
+
+
+class DeliveryProof(models.Model):
+    ...
---
+++ users/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ("Admin", "admin"),
+        ("Courier", "courier"),
+        ("Customer", "customer")
+    )
+
+
+    role = models.CharField(max_length=50, choices=ROLE_CHOICES)
