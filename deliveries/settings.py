262a974b71ad005f6749dcbc9030fca6cf26502f
onise2001 <onise.babulashvili@yahoo.com>
2024-06-17 19:51:19 +0400

    Changed database connection settings

--- deliveries/settings.py
+++ deliveries/settings.py
@@ -94,7 +94,7 @@ WSGI_APPLICATION = 'deliveries.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(os.getenv('DB_URL'))
+    'default': dj_database_url.parse(f'{os.getenv('DB_URL')}')
 }
 
 
