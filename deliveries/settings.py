db0e0bcec7171bf51e378184e4fc3ba9c45bfae2
onise2001 <onise.babulashvili@yahoo.com>
2024-06-18 12:48:56 +0400

    Collected static files

--- deliveries/settings.py
+++ deliveries/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
